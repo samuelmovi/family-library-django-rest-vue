57cffee9fc412c22ba78fbdbca1dfa599d4e71c3
sam <samuelmovi@protonmail.com>
2020-03-28 15:58:01 +0100

    some more fiddling, no luck

--- family_library_django_rest_vue/settings.py
+++ family_library_django_rest_vue/settings.py
@@ -97,7 +97,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
